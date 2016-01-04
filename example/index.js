d5514fa529d8a775504fa56d2c7614812ad075f7
Duncan Beaton <dunckr@gmail.com>
2016-01-04 21:22:46 +0000

    fix cleaner example with output of dom

--- example/index.js
+++ example/index.js
@@ -1,4 +1,4 @@
-import EmailObfuscate from '../src/emailObfuscate';
+import EmailObfuscate from '../src/emailObfuscate'
 
 var fontList = [
   'Arial',
@@ -9,7 +9,7 @@ var fontList = [
   'Trebuchet MS',
   'Times New Roman',
   'Verdana',
-];
+]
 
 var webFonts = [
   'Abril Fatface',
@@ -23,58 +23,70 @@ var webFonts = [
   'Roboto',
   'Ubuntu',
   'Volkhov'
-];
+]
 
 WebFont.load({
   google: {
     families: webFonts
   },
   active: () => {
-    fontList = fontList.concat(webFonts);
-    emailObfuscate();
-    generateSelection();
+    fontList = fontList.concat(webFonts)
+    emailObfuscate()
+    generateSelection()
   }
-});
+})
 
 var emailObfuscate = () => {
-  var el = document.getElementById('email');
-  EmailObfuscate(el, {});
+  var el = document.getElementById('obfuscated')
+  EmailObfuscate(el, {})
 }
 
 var generateStyle = (fontFamily) => {
-  var css = `.example a { font-family: "${fontFamily}" }`;
-  var style = document.createElement('style');
-  style.appendChild(document.createTextNode(css));
-  var exisitingStyle = document.head.getElementsByTagName('style');
+  var css = `.example a { font-family: "${fontFamily}" }`
+  var style = document.createElement('style')
+  style.appendChild(document.createTextNode(css))
+  var exisitingStyle = document.head.getElementsByTagName('style')
   if (exisitingStyle.length > 0) {
-    document.head.removeChild(exisitingStyle[0]);
+    document.head.removeChild(exisitingStyle[0])
   }
-  document.head.appendChild(style);
+  document.head.appendChild(style)
 }
 
-var fontSelectionEl = document.getElementById('fontSelection');
+var fontSelectionEl = document.getElementById('fontSelection')
 
 var generateSelection = () => {
   while (fontSelectionEl.firstChild) {
-    fontSelectionEl.removeChild(fontSelectionEl.firstChild);
+    fontSelectionEl.removeChild(fontSelectionEl.firstChild)
   }
   fontList.map((font) => {
-    var el = document.createElement('option');
-    el.innerHTML = font;
-    el.value = font;
-    fontSelectionEl.appendChild(el);
-  });
+    var el = document.createElement('option')
+    el.innerHTML = font
+    el.value = font
+    fontSelectionEl.appendChild(el)
+  })
+}
+
+var updateSource = (input, output) => {
+  var htmlStr = document.getElementById(input).outerHTML
+  var clearHtml = document.getElementById(output)
+  clearHtml.innerText = htmlStr
+}
+
+var updateSources = () => {
+  updateSource('clear', 'clearHtml')
+  updateSource('obfuscated', 'obfuscatedHtml')
 }
 
 fontSelectionEl.addEventListener('change', (e) => {
-  var fontFamily = e.target.value;
-  generateStyle(fontFamily);
-  emailObfuscate();
-});
+  var fontFamily = e.target.value
+  generateStyle(fontFamily)
+  emailObfuscate()
+})
 
 window.addEventListener('resize', () => {
-  emailObfuscate();
-});
+  emailObfuscate()
+})
 
-emailObfuscate();
-generateSelection();
+emailObfuscate()
+generateSelection()
+updateSources()
